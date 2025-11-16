083008425d9c78873da3c70395a679cbbb913aa4
RashLop <lopezmejiarasshid@outlook.com>
2025-11-16 19:50:56 -0400

    AppDbContext whit enitity User added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ProyectoTecWeb.Models;
+
+namespace ProyectoTecWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        public DbSet<User> users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>( u =>{
+                u.HasKey(u => u.Id);
+                u.Property(u => u.Email).IsRequired();
+                u.Property(u => u.Password).IsRequired();
+                u.Property(u => u.Name).IsRequired(); 
+                u.Property(u => u.Phone).IsRequired().HasMaxLength(8);
+                u.Property(u => u.Role).IsRequired().HasDefaultValue("User");
+                u.HasIndex(u => u.Email).IsUnique(); 
+            }); 
+        }
+    }
+}
