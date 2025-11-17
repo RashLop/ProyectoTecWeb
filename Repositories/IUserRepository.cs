d3d7b502f84b0a4ff3d6ea4b9527265395d8b629
RashLop <lopezmejiarasshid@outlook.com>
2025-11-16 20:13:10 -0400

    feat: UserRepository added and injection db added

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using ProyectoTecWeb.Models;
+
+namespace ProyectoTecWeb.Repository
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAddress(string email); 
+        Task AddAsync(User user); 
+        Task UpdateAsync(User user); 
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoTecWeb.Data;
+using ProyectoTecWeb.Models;
+
+namespace ProyectoTecWeb.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db; 
+        public UserRepository(AppDbContext db) => _db = db; 
+        public Task<User?> GetByEmailAddress(string email) => _db.users.FirstOrDefaultAsync(u => u.Email == email.Trim()); 
+        
+        public async Task AddAsync(User user)
+        {
+            _db.users.Add(user); 
+            await _db.SaveChangesAsync(); 
+        } 
+        public async Task UpdateAsync(User user)
+        {
+            _db.users.Update(user); 
+            await _db.SaveChangesAsync(); 
+        } 
+    }
+}
