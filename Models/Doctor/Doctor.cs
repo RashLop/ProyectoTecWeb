feece822df9b4ac9017c3470b4a23e047cb9fd78
RashLop <lopezmejiarasshid@outlook.com>
2025-11-18 10:19:46 -0400

    feat: ModelDoctors whit UseriD create and added

---
+++ Models/Doctor/Doctor.cs
@@ -0,0 +1,12 @@
+namespace ProyectoTecWeb.Models
+{
+    public class Doctor
+    {
+        public required User user {get; set; } 
+        public Guid UserId {get; set; } //fk user
+        public string Name {get; set; } = string.Empty; 
+        public string Phone {get; set; } = string.Empty; 
+        public string Specialty {get; set; } = string.Empty;
+
+    }
+}
