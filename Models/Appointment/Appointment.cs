c382402efd0a34cc30d73ab3f76353aec41ce0d5
unknown <oriana.flores@ucb.edu.bo>
2025-11-21 19:31:07 -0400

    fix: models appointment

--- Models/Appointment/Appointment.cs
+++ Models/Appointment/Appointment.cs
@@ -4,16 +4,13 @@
     {
         public Guid AppointmentId { get; set; }
 
-        // Doctor FK (solo ID por ahora)
+        // Foreign keys
         public Guid DoctorId { get; set; }
-        // public Doctor? Doctor { get; set; }
-
-        // Patient FK (igual, solo ID por ahora)
         public Guid PatientId { get; set; }
 
         // Datos de la cita
-        public DateTime Date { get; set; }      // Fecha
-        public TimeSpan Time { get; set; }      // Hora exacta
+        public DateTime Date { get; set; }      // Solo la fecha
+        public TimeSpan Time { get; set; }      // Solo la hora
 
         public string Reason { get; set; } = string.Empty;
 
@@ -26,8 +23,9 @@
 
         public string? Notes { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Doctor? Doctor { get; set; }
+        //public Patient? Patient { get; set; }
 
-        public Doctor? doctor {get; set;}
     }
 }
+
