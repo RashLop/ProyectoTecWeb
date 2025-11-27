b6d066f8d3388784ef217aca171c9abecc163c15
SealessPirate <ernes777rim@gmail.com>
2025-11-27 06:36:29 -0400

    Feat: Created History service interface

---
+++ Services/IHistoryService.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoTecWeb.Models.DTO;
+using ProyectoTecWeb.Models.History;
+using ProyectoTecWeb.Models.History.Dtos;
+namespace ProyectoTecWeb.Services
+{
+    public interface IHistoryService
+    {
+        Task<HistoryResponse?> GetOne(Guid id);
+        Task<IEnumerable<HistoryResponse>> GetAll();
+        Task<HistoryResponse> CreateAppointment(CreateHistoryDto dto);
+        Task<HistoryResponse?> Update(Guid id, UpdateHistoryDto dto);
+        Task Delete(Guid id);
+    }
+}
