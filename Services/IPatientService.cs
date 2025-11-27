b8088da7e33f2b39c3f6476770c6afd9b823cf2f
SealessPirate <ernes777rim@gmail.com>
2025-11-27 03:59:21 -0400

    Feat:Created Patient Service Interface

---
+++ Services/IPatientService.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoTecWeb.Models;
+using ProyectoTecWeb.Models.DTO;
+using ProyectoTecWeb.Models.Patient;
+using ProyectoTecWeb.Models.Patient.Dtos;
+
+namespace ProyectoTecWeb.Services
+{
+    public interface IPatientService
+    {
+        Task<PatientResponse> GetOnePatient(Guid id);
+        Task<IEnumerable<PatientResponse>> GetAllDoctors();
+
+        Task<Patient> CreateDoctor(CreatePatientDto dto);
+
+        Task<Patient> UpdateDoctor(UpdatePatientDto dto, Guid id);
+
+        Task Delete(Guid id);
+    }
+}
