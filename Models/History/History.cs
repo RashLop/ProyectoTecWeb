458ea5c814565f9571380accc7422c264bfcf678
SealessPirate <ernes777rim@gmail.com>
2025-11-27 05:41:27 -0400

    Feat:Created the model for a medical history belonging to Patient

---
+++ Models/History/History.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoTecWeb.Models.Patient;
+namespace ProyectoTecWeb.Models.History
+{
+    public class History
+    {
+        public Guid HistoryID { get; set; }
+
+        required public string BloodType { get; set; }
+        public ICollection<string>? Diagnoses { get; set; }
+        public ICollection<string>? Medication { get; set; }
+        public ICollection<string>? Allergies { get; set; }
+
+        required public ProyectoTecWeb.Models.Patient.Patient patient { get; set; }
+
+
+    }
+}
