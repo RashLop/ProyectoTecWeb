c0832a9a3014d4c1f247a07e7ce49f005407c520
SealessPirate <ernes777rim@gmail.com>
2025-11-27 06:54:49 -0400

    Feat: Added History service

---
+++ Services/HistoryService.cs
@@ -0,0 +1,51 @@
+﻿using ProyectoTecWeb.Models.DTO;
+using ProyectoTecWeb.Models.History;
+using ProyectoTecWeb.Models.History.Dtos;
+using ProyectoTecWeb.Repositories;
+using ProyectoTecWeb.Repository;
+namespace ProyectoTecWeb.Services
+{
+    public class HistoryService : IHistoryService
+    {
+        private readonly IHistoryRepository _repo;
+
+        public HistoryService(IHistoryRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<History> CreateHistory(CreateHistoryDto dto)
+        {
+            var history = new History {
+                HistoryID = Guid.NewGuid(),
+                BloodType = dto.BloodType,
+                PatientId = dto.PatientId
+            };
+            await _repo.AddAsync(history);
+            await _repo.SaveChangesAsync();
+            return history;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var byebye = await _repo.GetOneAsync(id);
+            if (byebye == null) throw new ArgumentException("Medical History not Found");
+            await _repo.DeleteAsync(byebye);
+            await _repo.SaveChangesAsync();
+        }
+
+        public async Task<History?> GetOne(Guid id)
+        {
+            var his = await _repo.GetOneAsync(id);
+            if (his == null) throw new ArgumentException("Medical History not Found");
+            return his;
+        }
+
+        public async Task<History> Update(Guid id, UpdateHistoryDto dto)
+        {
+            var updated = await _repo.GetOneAsync(id);
+            if (updated == null) throw new ArgumentException("Medical History not Found");
+            await _repo.UpdateAsync(updated);
+            return updated;
+        }
+    }
+}
